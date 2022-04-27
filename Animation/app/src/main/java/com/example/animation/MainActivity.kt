ecf7550039d8d01d8061b05271e42bac1643cf30
Franz <francesco.romeo-work@libero.it>
2022-04-27 14:28:24 +0200

    Finished Animation exercise and ViewBinfing exercise

--- Animation/app/src/main/java/com/example/animation/MainActivity.kt
+++ Animation/app/src/main/java/com/example/animation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.animation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,14 @@ class MainActivity : AppCompatActivity() {
 
         val animationView = findViewById<View>(R.id.animationView)
         animationView.alpha = 0f
-        animationView.animate().alpha(1f).setDuration(3000).start()
+        animationView.animate()
+            .alpha(1f)
+            .setDuration(3000)
+            .translationXBy(400f).start()
+
+        val animationButton = findViewById<Button>(R.id.animationButton)
+        animationButton.setOnClickListener {
+            animationView.animate().alpha(0f).setDuration(3000).translationXBy(160f).start()
+        }
     }
 }
---
+++ ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.testView.text = "Za Warudo!!!!"
+
+        binding.include.textInclude.text = "Test ZAWARDO!!!!"
+    }
+}

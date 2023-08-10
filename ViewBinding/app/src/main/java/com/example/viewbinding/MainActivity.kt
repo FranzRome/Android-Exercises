2eb7f7bb14b3dabc625f32749e500a1a29b6b63a
Franz <francesco.romeo-work@libero.it>
2023-08-11 01:36:19 +0200

    ViewBinding Exercise Update

--- ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
+++ ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -11,11 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.testView.text = "Za Warudo!!!!"
+        binding.text2.text = "FUKILIKI!"
 
         binding.include.textInclude.text = "Test ZAWARDO!!!!"
     }
